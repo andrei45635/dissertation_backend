ac6bfe606b4dff4b0e1402ed3499b71e1ad3e8d3
andrei45635 <iacob.andrei34@gmail.com>
2026-02-13 06:44:12 +0200

    properties

---
+++ init-db.sql
@@ -0,0 +1,14 @@
+-- Database initialization script
+-- This runs automatically when the PostgreSQL container starts for the first time
+
+-- Create extensions if needed
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Grant privileges (if using different users)
+-- GRANT ALL PRIVILEGES ON DATABASE msadetector TO postgres;
+
+-- Any initial data can be added here
+-- For example, a default admin user (password should be hashed in production)
+
+-- Note: Tables are created by Hibernate based on entity definitions
+-- This file is for any additional setup not covered by JPA
